8ba68d9150e5673c1c94630e6b98980d0fb68f06
Al1gazY <al1gazy@mail.ru>
2023-11-07 16:29:40 -0500

    Refactor Error Messages

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -14,28 +14,28 @@ class NetworkManager{
     
     private init() {}
     
-    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String?) -> Void) {
+    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, ErrorMessages?) -> Void) {
         let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
         
         guard let url = URL(string: endpoint) else {
-            completed(nil, "Error, Please Try Again")
+            completed(nil, .invalidUsername)
             return
         }
         
         let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
                 
             if let _ = error{
-                completed(nil, "Error, Please check your internet connection.")
+                completed(nil, .unableToComplete)
                 return
             }
             
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(nil, "Error, Invalid responce from the server.")
+                completed(nil, .invalidResponce)
                 return
             }
             
             guard let data = data else{
-                completed(nil, "Error, The data was invalid.")
+                completed(nil, .invalidData)
                 return
             }
             
@@ -45,7 +45,7 @@ class NetworkManager{
                 let followers = try decoder.decode([Follower].self, from: data)
                 completed(followers, nil)
             } catch{
-                completed(nil, "Error, The data was invalid.")
+                completed(nil, .invalidData)
             }
         }
         
---
+++ GHFollowers/Utilities/ErrorMessages.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessages.swift
+//  GHFollowers
+//
+//  Created by Aligazy Kismetov on 07.11.2023.
+//
+
+import Foundation
+
+enum ErrorMessages: String{
+    case invalidUsername = "Error. Invalid username."
+    case unableToComplete = "Error. Please check your internet connection."
+    case invalidResponce = "Error. Invalid responce from the server."
+    case invalidData = "Error. The data was invalid."
+}
