8997766f251b8c4b2c76bf12ebe3c7909a89d1eb
Al1gazY <al1gazy@mail.ru>
2023-11-08 18:45:08 -0500

    Downloading and Caching avatars

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,12 +5,13 @@
 //  Created by Aligazy Kismetov on 06.11.2023.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager{
     
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
