45e1426855c8280a9a641570f49b05d0974e70f1
Al1gazY <al1gazy@mail.ru>
2023-11-12 20:01:33 -0500

    Favorites VC part 1

---
+++ GHFollowers/Custom Views/ViewControllers/GFDataLoadingVC.swift
@@ -0,0 +1,29 @@
+//
+//  GFDataLoadingVC.swift
+//  GHFollowers
+//
+//  Created by Aligazy Kismetov on 12.11.2023.
+//
+
+import UIKit
+
+class GFDataLoadingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
