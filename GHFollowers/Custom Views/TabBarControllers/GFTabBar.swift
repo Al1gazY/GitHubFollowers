dabed4d5aa6946c75f0dee20c5713268b8dc2fd0
Al1gazY <al1gazy@mail.ru>
2023-11-12 20:31:40 -0500

    TabBar clearance

---
+++ GHFollowers/Custom Views/TabBarControllers/GFTabBar.swift
@@ -0,0 +1,36 @@
+//
+//  GFTabBar.swift
+//  GHFollowers
+//
+//  Created by Aligazy Kismetov on 12.11.2023.
+//
+
+import UIKit
+
+class GFTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNC(), createFavoritesNC()]
+
+    }
+
+    func createSearchNC() -> UINavigationController{
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    
+    func createFavoritesNC() -> UINavigationController{
+        let favoritesVC = FavoritesListVC()
+        favoritesVC.title = "Favorites"
+        favoritesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesVC)
+    }
+
+}
